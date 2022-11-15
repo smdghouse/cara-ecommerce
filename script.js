18081bae4194e8226c09e330afb1c1feaa4ba30e
ghouse <smdghouse2001@gmail.com>
2022-11-15 22:12:45 +0530

    single product completed

--- script.js
+++ script.js
@@ -24,3 +24,39 @@ close.onclick=()=>
         nav.style.right="-300px"
     }
 }
+//-----------------------------> for single product
+
+var smallimg = document.getElementsByClassName(" smallimg")
+var bigimg = document.getElementById("bigimg")
+
+smallimg[0].onclick=()=>
+{
+    bigimg.src=smallimg[0].src
+}
+smallimg[1].onclick=()=>
+{
+    bigimg.src=smallimg[1].src
+}
+smallimg[2].onclick=()=>
+{
+    bigimg.src=smallimg[2].src
+}
+smallimg[3].onclick=()=>
+{
+    bigimg.src=smallimg[3].src
+}
+// for redirect to product page
+
+// var productimg=document.getElementsByClassName("product-img")
+
+// productimg.onclick=()=>
+// {
+//     for(var i =1;i<4;i++)
+//     {
+//         if(productimg[i].src=!bigimg.src)
+//         {
+//             bigimg.src=productimg[i].src
+//         }
+//     }
+// }
+
